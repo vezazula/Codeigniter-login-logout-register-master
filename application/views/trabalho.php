4ab82ce60016133175a9244489aa188c474a8c3e
vezazula <veronicazazula@gmail.com>
2016-07-01 00:39:28 -0300

    upd

---
+++ application/views/trabalho.php
@@ -0,0 +1,111 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<section id="services">
+<div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Trabalhos Realizados</h2>
+                    <h2 class="section-heading">Algumas imagens de Sistemas/Projetos realizados</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-diamond text-primary sr-icons"></i>
+                        <h3>C#.net</h3>
+                        <p class="text-muted">Conhecimento médio, obtido através de curso</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-paper-plane text-primary sr-icons"></i>
+                        <h3>HTML</h3>
+                        <p class="text-muted">Conhecimento médio, obtido através da faculdade.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
+                        <h3>C</h3>
+                        <p class="text-muted">Conhecimento médio, obtido através da faculdade.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
+                        <h3>Arduino</h3>
+                        <p class="text-muted">Conhecimento básico, obtido através da faculdade.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter popup-gallery">
+                <div class="col-lg-6 col-sm-6">
+                    <a href="<?= base_url('assets/img/portfolio/fullsize/c#.jpg') ?>" class="portfolio-box">
+                        <img src="<?= base_url('assets/img/portfolio/thumbnails/c#.jpg') ?>" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <a href="<?= base_url('assets/img/portfolio/fullsize/1.jpg') ?>" class="portfolio-box">
+                        <img src="<?= base_url('assets/img/portfolio/thumbnails/1.jpg') ?>" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <a href="<?= base_url('assets/img/portfolio/fullsize/1.jpg') ?>" class="portfolio-box">
+                        <img src="<?= base_url('assets/img/portfolio/thumbnails/1.jpg') ?>" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <a href="<?= base_url('assets/img/portfolio/fullsize/ard.jpg') ?>" class="portfolio-box">
+                        <img src="<?= base_url('assets/img/portfolio/thumbnails/ard.jpg') ?>" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
